2cfca5435b5308354055a633211e462e1eee2f2f
mango <adamtamers@outlook.com>
2021-01-17 00:04:24 +0400

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,7 +1,17 @@
-import pyautogui, time
+import pyautogui
+import time
+import keyboard
+
 time.sleep(3)
-f = open("shrekmovie.txt", 'r')
+
+f = open("shrekmovie.txt", 'r') #PHYSICAL SPAM SCRIPT
 for word in f:
     time.sleep(0.50)
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+
+while True: #STANDARD COMBO PROCEDURE 
+    if keyboard.is_pressed('CTRL'):
+        if keyboard.is_pressed('X'):
+            break #STOP SCRIPT WHEN KEYBIND TOGGLED
+            
