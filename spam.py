437f7f3b916921d79afbec111a364bd61e7b2e1f
mango <adamtamers@outlook.com>
2021-05-18 22:03:46 +0400

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,17 +1,18 @@
-import pyautogui
-import time
-import keyboard
+import pyautogui # For typing!
+import time # So it doesn't write the entire thing at once!
+import keyboard # So you can stop the program!
 
 time.sleep(3)
 
-f = open("shrekmovie.txt", 'r') #PHYSICAL SPAM SCRIPT
-for word in f:
-    time.sleep(0.50)
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
-
-while True: #STANDARD COMBO PROCEDURE 
-    if keyboard.is_pressed('CTRL'):
-        if keyboard.is_pressed('X'):
-            break #STOP SCRIPT WHEN KEYBIND TOGGLED
+def shrek():
+    while True: #STANDARD COMBO PROCEDURE 
+        if keyboard.is_pressed('SHIFT'):
+            if keyboard.is_pressed('Z'):
+                print("Script Cancelled!")
+                break #STOP SCRIPT WHEN KEYBIND TOGGLED
+        if = open("shrekmovie.txt", 'r') # Open the file
+        for word in f:
+            time.sleep(0.50)
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
             
